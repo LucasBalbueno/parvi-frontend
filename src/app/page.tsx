284895ec9147045d223712197ba44b658cf990c5
LucasBalbueno <balbuenolucas04@gmail.com>
2025-10-07 23:06:38 -0300

    Config font and colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,33 @@ export default function Home() {
   return (
     <div>
       <main>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, reprehenderit?</p>
+        <p className="py-3 font-title uppercase text-gradient-purple ">Lorem ipsum</p>
+
+        <p className="py-3 font-extralight text-darkpurple">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, reprehenderit?
+        </p>
+        <p className="py-3 font-light text-greensuccess">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, reprehenderit?
+        </p>
+        <p className="py-3 font-normal text-redalert">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, reprehenderit?
+        </p>
+        <p className="py-3 font-medium text-whiteinput">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, reprehenderit?
+        </p>
+        <p className="py-3 font-semibold text-gradient-purple">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, reprehenderit?
+        </p>
+        <p className="py-3 font-bold">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, reprehenderit?
+        </p>
+        <p className="py-3 font-extrabold">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, reprehenderit?
+        </p>
+        <p className="py-3 font-black">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, reprehenderit?
+        </p>
+        <div className="py-5 w-10/12 h-20 bg-gradient-purple"></div>
       </main>
     </div>
   );
